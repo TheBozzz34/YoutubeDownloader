ffb541c281cd166eac5e99496c33f3df2ae4312b
Ethan James <lskywalker416@gmail.com>
2022-06-14 19:26:24 -0600

    first commit

---
+++ build/backend/index.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+var cors = require('cors');
+const { exec } = require("child_process");
+const path = require('path')
+
+var public = path.join(__dirname, 'uploads');
+// use it before all route definitions
+
+
+
+// Create a new instance of express
+const app = express()
+app.use(cors())
+// Tell express to use the body-parser middleware and to not parse extended bodies
+app.use(bodyParser.urlencoded({ extended: false }))
+app.options('*', cors())
+
+app.post('/download', (req, res) => {
+    //console.log('Got body:', req.body);
+    str = JSON.stringify(req.body);
+    var result = JSON.parse(str);
+    console.log( "Starting download of " + result.url)
+    var finalUrl = result.url
+    res.sendStatus(200);
+
+    exec('yt-dlp -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best" -P D:/YouTubeDownloaderHTML/express-app/uploads ' + finalUrl, (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+    });
+
+});
+
+
+// Tell our app to listen on port 3000
+app.listen(3001, function (err) {
+  if (err) {
+    throw err
+  }
+
+  app.get('/uploads', function(req, res) {
+    res.sendFile(path.join(public, 'index.html'));
+});
+
+app.use('/uploads', express.static(public));
+
+  console.log('Server started on port 3001')
+})
---
+++ vue-project/src/main.ts
@@ -0,0 +1,78 @@
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import axios from 'axios'
+import qs from 'qs';
+
+import App from "./App.vue";
+import router from "./router";
+
+const app = createApp(App);
+
+
+app.use(createPinia());
+app.use(router);
+
+app.mount("#app");
+
+
+
+
+const el = document.getElementById("video-input-button")
+// @ts-ignore: Object is possibly 'null'.
+el.addEventListener("click", download);
+
+function download() {
+
+    var vidurl = (<HTMLInputElement>document.getElementById("video-input")).value;
+    axios({
+        method: 'POST',
+        url: 'http://localhost:3001/download',
+        data: qs.stringify({
+          url: vidurl,
+        }),
+      });
+
+    
+    /*
+      async function postData() {
+
+        var vidurl = (<HTMLInputElement>document.getElementById("video-input")).value;
+        try {
+
+          // 👇️ const response: Response
+        
+          const response = await fetch('http://localhost:3001/download', {
+            method: 'POST',
+            body: ,
+            headers: {
+              'Content-Type': 'application/x-www-form-urlencoded',
+              Accept: 'application/x-www-form-urlencoded',
+              'Access-Control-Allow-Origin': "*"
+            },
+          });
+      
+          if (!response.ok) {
+            throw new Error(`Error! status: ${response.status}`);
+          } else {
+              console.log("sent post request!")
+          }
+       
+
+        } catch (error) {
+          if (error instanceof Error) {
+            console.log('error message: ', error.message);
+            return error.message;
+          } else {
+            console.log('unexpected error: ', error);
+            return 'An unexpected error occurred';
+          }
+        }
+      }
+      
+      postData();
+    */
+   
+}
+
+
+
